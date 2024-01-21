1ba86f96a1be3ce111f7668209a92acd61dd48d9
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-21 16:53:53 +0500

    controllers added

---
+++ GidGroupBackend/src/GidGroup.Web/Controllers/CatalogController.cs
@@ -0,0 +1,38 @@
+﻿using GidGroup.Application.UseCases.Catalogs.Commands;
+using GidGroup.Application.UseCases.Catalogs.Queries;
+using GidGroup.Domain.Entities;
+using GidGroup.Web.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GidGroup.Web.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CatalogController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateCatalog(CatalogDTO catalogDTO)
+        {
+            var catalog = new CreateCatalogCommand()
+            {
+                Email = catalogDTO.Email,
+            };
+            await _mediator.Send(catalog);
+            return Ok("Created");
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllCatalog()
+        {
+            IList<GetCatalog> catalogs = await _mediator.Send(new GetCatalogQuery());
+            return Ok(catalogs);
+        }
+    }
+}
