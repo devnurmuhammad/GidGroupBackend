7f4f0f03827297ebcbcb6ab79ee38d3f2ce17a5a
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-21 14:20:44 +0500

    Web layer added

--- GidGroupBackend/src/GidGroup.Web/Program.cs
+++ GidGroupBackend/src/GidGroup.Web/Program.cs
@@ -1,3 +1,7 @@
+using GidGroup.Application;
+using GidGroup.Infrastructure;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
