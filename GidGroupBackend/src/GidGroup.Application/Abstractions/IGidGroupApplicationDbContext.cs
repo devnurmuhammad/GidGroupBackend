6ae6b957d3b83b583931d260c04ce49dece198a6
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-21 14:20:13 +0500

    Application layer added

---
+++ GidGroupBackend/src/GidGroup.Application/Abstractions/IGidGroupApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GidGroup.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GidGroup.Application.Abstractions
+{
+    public interface IGidGroupApplicationDbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Recall> Recalls { get; set; }
+        public DbSet<GetCatalog> GetCatalogs { get; set; }
+        public DbSet<ContactUs> ContactUs { get; set; }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ GidGroupBackend/src/GidGroup.Application/GidGroupDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace GidGroup.Application
+{
+    public static class GidGroupDependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
