603c07d01196a2048e4ecbf9df91359a19004be4
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-21 16:53:05 +0500

    catalogs' added

---
+++ GidGroupBackend/src/GidGroup.Application/UseCases/Catalogs/Commands/CreateCatalogCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GidGroup.Application.UseCases.Catalogs.Commands
+{
+    public class CreateCatalogCommand : IRequest
+    {
+        public string Email { get; set; } = default!;
+    }
+}
