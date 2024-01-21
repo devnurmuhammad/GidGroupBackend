627d0b45334eb0ea2df247d6b6a34ced36d1f745
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-21 15:03:01 +0500

    connected to the database)

---
+++ GidGroupBackend/src/GidGroup.Application/UseCases/Recalls/Handlers/CreateRecallHandler.cs
@@ -0,0 +1,28 @@
+﻿using GidGroup.Application.Abstractions;
+using GidGroup.Application.UseCases.Recalls.Commands;
+using GidGroup.Domain.Entities;
+using MediatR;
+
+namespace GidGroup.Application.UseCases.Recalls.Handlers
+{
+    public class CreateRecallHandler : AsyncRequestHandler<CreateRecallCommand>
+    {
+        private readonly IGidGroupApplicationDbContext _context;
+
+        public CreateRecallHandler(IGidGroupApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        protected override async Task Handle(CreateRecallCommand request, CancellationToken cancellationToken)
+        {
+            Recall recall = new Recall()
+            {
+                Name = request.Name,
+                Phone = request.Phone,
+            };
+            await _context.Recalls.AddAsync(recall);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
