708a3b8228f6cd0a9267d0ffac95b1607bca5f20
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-01-21 18:42:26 +0500

    orders' handlers added

---
+++ GidGroupBackend/src/GidGroup.Application/UseCases/Orders/Commands/CreateOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using GidGroup.Domain.Enums;
+using MediatR;
+
+namespace GidGroup.Application.UseCases.Orders.Commands
+{
+    public class CreateOrderCommand : IRequest
+    {
+        public string Size { get; set; } = default!;
+        public string Style { get; set; } = default!;
+        public string WhenNeed { get; set; } = default!;
+        public string Phone { get; set; } = default!;
+        public string? Email { get; set; }
+        public DateTime Date { get; set; }
+        public StatusEnums.Status Status { get; set; }
+    }
+}
